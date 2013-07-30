0e123ffa5aa9fddf7baff08b2cbb8f240d494f77
robertmryan <robert.ryan@mindspring.com>
2013-07-30 13:15:26 -0400

    Initial Commit

---
+++ Web Request Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Web Request Demo
+//
+//  Created by Robert Ryan on 7/30/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Web Request Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Web Request Demo
+//
+//  Created by Robert Ryan on 7/30/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
